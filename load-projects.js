64b3961a2a10dcf8509371a43c24fe5626b888d1
Bobby Gehr <43101950+EliteBAM@users.noreply.github.com>
2025-05-14 21:06:09 -0700

    still not working
    testing

--- load-projects.js
+++ load-projects.js
@@ -60,6 +60,8 @@ window.addEventListener('DOMContentLoaded', () => {
                 //modalCode.innerText = `// Example code for ${project.title}\nconsole.log("${project.title} clicked!");`;
     
                 // load template layout + data on-click from the serverless function
+                console.log("Loading project:", project.title);
+
                 fetch(`/api/project-details?title=${encodeURIComponent(project.title)}`)
                     .then(response => response.json())
                     .then(projectData => {
