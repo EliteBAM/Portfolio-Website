f9eeb72cc31f1523ea4e5c18b76079c5e169f8f6
Bobby Gehr <43101950+EliteBAM@users.noreply.github.com>
2025-05-14 23:42:52 -0700

    t

--- api/project-details.js
+++ api/project-details.js
@@ -13,10 +13,8 @@ export default async function handler(req, res) {
 
     //Extracting order of template IDs, as they correlate to order of project-details content sub-folders
     const templateOrderFilePath = path.join(projectDetailsDir, 'templates.txt');
-    const templateIDs = [];
-    const templateIDList = [];
-        templateIDs = await fs.readFile(templateOrderFilePath, 'utf-8');
-        templateIDList = templateIDs.split(',').map(templateID => templateID.trim()); //array of template ID order
+    const templateIDs = await fs.readFile(templateOrderFilePath, 'utf-8');
+    const templateIDList = templateIDs.split(',').map(templateID => templateID.trim()); //array of template ID order
 
 
     // Only get folders (ignore templates.txt and other files)
