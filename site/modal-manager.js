a0ddf48036ef0f88da00dd69602483f296d6bb99
Bobby Gehr <43101950+EliteBAM@users.noreply.github.com>
2025-06-08 00:29:45 -0700

    removing legacy code

--- site/modal-manager.js
+++ site/modal-manager.js
@@ -1,6 +1,8 @@
 let projectMap = new Map(); //global dictionary mapping a project to its modal details page
 //KEY: Project Title, VALUE: Downloaded modal page
 
+createProjectMapFromJSON();
+
 function createProjectMapFromJSON() {
     fetch(`/data/project-details.json`)
         .then(response => response.json())
@@ -10,9 +12,5 @@ function createProjectMapFromJSON() {
             console.log("project Map created: ", projectMap);
         })
         .catch(error => console.error('Error getting project details from JSON:', error));
-}
-
-function addProjectEntry (project) {
-    projectMap.set(project, null);
 }
 
