d71b0a0a2618b23acec22d068575f400d6781771
Bobby Gehr <43101950+EliteBAM@users.noreply.github.com>
2025-07-04 16:22:36 -0700

    mobile active project fixes

--- site/mobile-features.js
+++ site/mobile-features.js
@@ -6,10 +6,10 @@ if(isTouchDevice) {
     window.addEventListener('scroll', () => {
         document.querySelectorAll('.project').forEach(projectElement => {
             const rect = projectElement.getBoundingClientRect();
-            const centerY = window.innerHeight / 2;
+            const centerY = (window.innerHeight / 3) * 2;
             const isActive = rect.top < centerY && rect.bottom > centerY;
-            if(isActive)
-                projectElement.classList.toggle('active', isActive);
+
+            projectElement.classList.toggle('active', isActive);
         });
     });
 }
